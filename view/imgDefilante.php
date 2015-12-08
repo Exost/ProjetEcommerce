cd460ee245cc9d299d7767a60dd80aa4e021dd41
enzo <enzo.fabre@orange.fr>
2015-12-08 10:41:33 +0100

    Css et mis en place d'un diaporama

--- view/imgDefilante.php
+++ view/imgDefilante.php
@@ -1,19 +1,23 @@
 <section id="slideshow">
     <div class="container">
-        <?php
+        <div class="c_slider"></div>
+        <div class="slider">
+            <?php
             $pathImg ="{$ROOT}{$DS}ressources{$DS}img{$DS}slide";
-        //scandir renvoi une array avec tout les nom des fichier d'un repertoire
+            //scandir renvoi une array avec tout les nom des fichier d'un repertoire
             foreach (scandir($pathImg) as $f){
                 if($f !='.'& $f !='..'){
                     $img=$f; ?>
 
-        <figure>
-            <img src=<?php echo "ressources/img/slide/$img" ?> class="imageSlide"/>
-        </figure>
-                <?php
+                    <figure>
+                        <img src=<?php echo "ressources/img/slide/$img" ?> class="imageSlide"/>
+                    </figure>
+                    <?php
+                }
             }
-        }
-        ?>
+            ?>
 
+        </div>
     </div>
+    <span id="time"></span>
 </section>
