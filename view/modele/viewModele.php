a8705e06521b73a3ea31c64c88c06a4efedd7795
enzo <enzo.fabre@orange.fr>
2015-12-08 23:41:40 +0100

    Diaporama fini choix taille article sur le modèle

--- view/modele/viewModele.php
+++ view/modele/viewModele.php
@@ -1,48 +1,49 @@
 
 
 <?php
-/**
- * Created by PhpStorm.
- * User: enzo
- * Date: 26/10/15
- * Time: 16:41
- */
 $idModSansEspace=str_replace(' ','_',$modele->getNameMod()); // permet d'enlever les espace pour retrouver le nom des img
-echo "<img src='ressources{$DS}img{$DS}modele{$DS}{$idModSansEspace}.jpg' alt='{$idModSansEspace}'
-            style='margin-left:100px; width:55%%;height:350px;'><p></p>"; // load the image associated
-echo "{$idModSansEspace}</Br>
-        {$modele->getDescMod()}</Br>";
-
-echo "Prix: {$modele->getPriceMod()} euros</Br>";
-echo "Marque: {$modele->getNameBrand()}
-        <i><a href='index.php?controller=brand&action=read&idBrand={$modele->getNameBrand()}'>
-           voir marque</a></i>";
-
-if(!empty($tabItem)){
-
-
+$img = "ressources{$DS}img{$DS}modele{$DS}{$idModSansEspace}.jpg";
+$alt = "{$idModSansEspace}";
 ?>
-
-<form>
-    <select  name="taille" id="category">
-        <?php
+<div class="block">
+    <img src=<?php echo $img;?> alt=<?php echo $alt;?> class="image">
+</div>
+<?php
+if(!empty($tabItem)){
+    ?>
+    <div  class="block">
+    <form>
+        <select  name="taille" id="category">
+            <?php
             foreach($tabItem as $item){
                 echo "<option value='{$item->getSizeItem()}'>{$item->getSizeItem()}</option>";
 
             }
-        ?>
-    </select>
-    <select>
-        <?php
-        foreach($tabItem as $item){
-            echo "<option value=''>{$item->getColorItem()}</option>";
+            ?>
+        </select>
+        <select>
+            <?php
+            foreach($tabItem as $item){
+                echo "<option value=''>{$item->getColorItem()}</option>";
 
-        }
+            }
 
-        ?>
-    </select>
-</form>
-<?php
+            ?>
+        </select>
+    </form>
+    <?php
 }else {?>
-<div style="color: red; text-align: center;">produit épuisé</div>
+    <div class="block">produit épuisé</div>
+    </div>
 <?php } ?>
+<p></p>
+<div>
+    <div><?php echo "$idModSansEspace" ?><br/> </div>
+            <?php echo $modele->getDescMod();?><br/>
+    Prix: <?php echo $modele->getPriceMod();?><br/>
+    Marque: <?php echo "<a href='index.php?controller=brand&action=read&idBrand={$modele->getNameBrand()}'
+            >{$modele->getNameBrand()}</a>"; ?>
+</div>
+
+
+
